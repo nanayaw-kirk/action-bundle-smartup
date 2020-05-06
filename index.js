4c6bf799295820ed7d344ac2c3615e160d563937
Michał Zarach <michal.m.zarach@gmail.com>
2020-05-06 13:21:55 +0200

    fit action to electron-nuxt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { execSync } = require("child_process");
-const { existsSync, readFileSync } = require("fs");
+const { existsSync } = require("fs");
 const { join } = require("path");
 
 /**
@@ -66,21 +66,9 @@ const getInput = (name, required) => {
 const runAction = () => {
 	const platform = getPlatform();
 	const release = getInput("release", true) === "true";
-	const pkgRoot = getInput("package_root", true);
-	const buildScriptName = getInput("build_script_name", true);
-	const skipBuild = getInput("skip_build") === "true";
-	const useVueCli = getInput("use_vue_cli") === "true";
-
-	// TODO: Deprecated option, remove in v2.0. `electron-builder` always requires a `package.json` in
-	// the same directory as the Electron app, so the `package_root` option should be used instead
-	const appRoot = getInput("app_root") || pkgRoot;
+	const pkgRoot = ".";
 
 	const pkgJsonPath = join(pkgRoot, "package.json");
-	const pkgLockPath = join(pkgRoot, "package-lock.json");
-
-	// Determine whether NPM should be used to run commands (instead of Yarn, which is the default)
-	const useNpm = existsSync(pkgLockPath);
-	log(`Will run ${useNpm ? "NPM" : "Yarn"} commands in directory "${pkgRoot}"`);
 
 	// Make sure `package.json` file exists
 	if (!existsSync(pkgJsonPath)) {
@@ -103,34 +91,11 @@ const runAction = () => {
 	// Disable console advertisements during install phase
 	setEnv("ADBLOCK", true);
 
-	log(`Installing dependencies using ${useNpm ? "NPM" : "Yarn"}…`);
-	run(useNpm ? "npm install" : "yarn", pkgRoot);
-
-	// Run NPM build script if it exists
-	if (skipBuild) {
-		log("Skipping build script because `skip_build` option is set");
-	} else {
-		log("Running the build script…");
-		if (useNpm) {
-			run(`npm run ${buildScriptName} --if-present`, pkgRoot);
-		} else {
-			// TODO: Use `yarn run ${buildScriptName} --if-present` once supported
-			// https://github.com/yarnpkg/yarn/issues/6894
-			const pkgJson = JSON.parse(readFileSync(pkgJsonPath, "utf8"));
-			if (pkgJson.scripts && pkgJson.scripts[buildScriptName]) {
-				run(`yarn run ${buildScriptName}`, pkgRoot);
-			}
-		}
-	}
+	log(`Installing dependencies using…`);
+	run("yarn install", pkgRoot);
 
 	log(`Building${release ? " and releasing" : ""} the Electron app…`);
-	const cmd = useVueCli ? "vue-cli-service electron:build" : "electron-builder";
-	run(
-		`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
-			release ? "--publish always" : ""
-		}`,
-		appRoot,
-	);
+	run( `yarn run build ${(release ? "-- --publish always" : "")}`, pkgRoot);
 };
 
 runAction();
